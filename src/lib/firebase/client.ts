bfc240b8911267cb49adc694c080917032bd1568
Muhaimin Kpm <muhaiminkpm999@gmail.com>
2025-09-11 17:03:10 +0000

    Connect Firebase Authentication and Firestore Database directly in Fireb

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -3,12 +3,12 @@ import {getAuth} from 'firebase/auth';
 import {getFirestore} from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: 'AIzaSyCvi-Wp7wtl2y5aEcWcekm14CTjj0EiGG4',
+  authDomain: 'studio-5759297052-61ee5.firebaseapp.com',
+  projectId: 'studio-5759297052-61ee5',
+  storageBucket: 'studio-5759297052-61ee5.appspot.com',
+  messagingSenderId: '542214912337',
+  appId: '1:542214912337:web:540de702032829801f7b70',
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
